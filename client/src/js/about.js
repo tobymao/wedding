804458ca502e7fb53a68e18ef7f1db1292869f9a
Toby Mao <toby.mao@gmail.com>
2015-01-21 19:36:40 -0800

    added js lib

--- client/src/js/about.js
+++ client/src/js/about.js
@@ -2,12 +2,27 @@
 // You need this to create html views.
 
 var React = require('react');
+var imageSlider = require('./libs/js-image-slider.js');
 
 var AboutView = React.createClass({
   render: function() {
     /*jshint ignore:start */
     return (
-      <p>About Me!</p>
+      <div>
+        <h1>About Us</h1>
+        <div id="sliderFrame">
+          <div id="slider">
+            <img src = "../wedding/img/kirby1.jpg" alt="Toby and Ambie met through the Rubik's cube." /> 
+            <img src = "../wedding/img/kirby2.jpg" /> 
+            <img src = "../wedding/img/kirby3.jpg" /> 
+            <img src = "../wedding/img/kirby4.jpg" /> 
+          </div>
+          <div className="group1-Wrapper">
+            <a onClick={imageSlider.previous} className="group1-Prev"></a>
+            <a onClick={imageSlider.next} className="group1-Next"></a>
+          </div>
+        </div>
+      </div>
     );
     /*jshint ignore:end */
   },
--- client/src/js/rsvp.js
+++ client/src/js/rsvp.js
@@ -2,26 +2,16 @@
 // You need this to create html views.
 
 var React = require('react');
+require('./libs/js-image-slider.js');
 
 var RSVPView = React.createClass({
   render: function() {
     /*jshint ignore:start */
     return (
-      <div>
-      <form>
-        <label for="Name">Name:</label> <input type="text" name="Name" /><br />
-        <label for="Age">Over 21?</label> <select name="Age">
-          <option value="Adult">Yes</option>
-          <option value="Child">No</option>
-          </select><br />
-        <label for="Attending">Can you come to the wedding?</label> <select name="Attending">
-          <option value="Yes">Yes</option>
-          <option value="No">No</option>
-          </select><br />
-        <label for="allergies">Food Allergies:</label> <input type="text" name="allergies" /><br />
-        <input type="submit" value = "Submit" />
-      </form>
-      </div>
+        <div>
+	Blah.
+        </div>
+
     );
     /*jshint ignore:end */
   },
