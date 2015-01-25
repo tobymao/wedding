b98dc9f8ce60d83a03758ad79d5b411d3e330441
Toby Mao <toby.mao@gmail.com>
2015-01-25 12:46:31 -0800

    added faq

--- client/src/js/faq.js
+++ client/src/js/faq.js
@@ -8,8 +8,19 @@ var FAQView = React.createClass({
     /*jshint ignore:start */
     return (
       <div>
-      <h1>Frequently Asked Questions</h1>
-      <p>Coming soon.</p>
+        <h1>Frequently Asked Questions</h1>
+        <p className="question">What should we wear?</p>
+        <p className="answer">The dress code is casual. You can wear whatever you like.</p>
+        <p className="question">Is it really casual?</p>
+        <p className="answer">Yup!</p>
+        <p className="question">So I can wear what I'm wearing now?</p>
+        <p className="answer">Yes, as long as it's appropriate to go out in public.</p>
+        <p className="question">Will you have a registry?</p>
+        <p className="answer">No, we will not.</p>
+        <p className="question">Will there be dancing?</p>
+        <p className="answer">Well will not have dancing at our wedding, but we are planning on having other fun activities for you to do!</p>
+        <p className="question">Will there be alcohol?</p>
+        <p className="answer">We plan on having food (buffet) and drinks (both alcoholic and non-alcoholic) during the reception after the ceremony.</p>
       </div>
     );
     /*jshint ignore:end */
